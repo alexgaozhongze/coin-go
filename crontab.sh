61e3737e8b9bb18f57833bd3ef1c00eb873256f2
alex <alexgaozhongze@gmail.com>
2021-09-10 18:20:15 +0800

    go Beyond

--- crontab.sh
+++ crontab.sh
@@ -2,7 +2,7 @@
 ps -ef | grep go | grep coin-usdt | grep -v grep
 if [ $? -ne 0 ]
 then
-/root/go/src/coin-v5/coin-usdt >> /tmp/coin.log
+/root/go/src/coin-v6/coin-usdt >> /tmp/coin.log
 else
 echo "runing......"
 fi
