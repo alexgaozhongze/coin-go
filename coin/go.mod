28809b9045058d684c5b9a6cdc9376af4bab2ea1
alex <alexgaozhongze@gmail.com>
2021-09-29 15:39:25 +0800

    go Beyond

--- coin/go.mod
+++ coin/go.mod
@@ -2,4 +2,4 @@ module coin-ht
 
 go 1.16
 
-require github.com/huobirdcenter/huobi_golang v0.0.0-20210226095227-8a30a95b6d0d // indirect
+require github.com/huobirdcenter/huobi_golang v0.0.0-20210226095227-8a30a95b6d0d
--- coin/main.go
+++ coin/main.go
@@ -1,34 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
-	"syscall"
-	"sync"
-	"time"
+	"regexp"
+	"sort"
 	"strconv"
-	"fmt"
-	// "reflect"
+	"sync"
+	"syscall"
+	"time"
+
 	"github.com/huobirdcenter/huobi_golang/config"
+	"github.com/huobirdcenter/huobi_golang/logging/applogger"
 	"github.com/huobirdcenter/huobi_golang/pkg/client"
+	"github.com/huobirdcenter/huobi_golang/pkg/model/common"
 	"github.com/huobirdcenter/huobi_golang/pkg/model/market"
 	"github.com/huobirdcenter/huobi_golang/pkg/model/order"
-	"github.com/huobirdcenter/huobi_golang/pkg/model/common"
-	"github.com/huobirdcenter/huobi_golang/logging/applogger"
-  )
+)
 
-const PRICENUM int = 36
-const CLOSENUM int = 999
+const CLOSENUM int = 36
 const AMOUNTUSDT int = 9
   
 var wg sync.WaitGroup
 var wgKline sync.WaitGroup
 
+var CommonClient *client.CommonClient
+var MarketClient *client.MarketClient
+var AccountClient *client.AccountClient
+var OrderClient *client.OrderClient
+
+var symbols []string
 var symbolBuy = make(map[string]int)
 var symbolInfo = make(map[string]common.Symbol)
 var symbolAmount = make(map[string]float64)
-var symbolEma = make(map[string]Ema)
-var symbolEmaTimes = make(map[string]int)
+var symbolEma = make(map[string][]Ema)
 
 var exit bool
 
@@ -46,13 +52,19 @@ type OrderInfo struct {
 }
 
 type Ema struct {
-	Ema39 	float64
-	Ema66 	float64
-	Ema93 	float64
-	Ema333	float64
-	Ema666	float64
-	Ema999	float64
+	Id 		int64
+	Close	float64
+	Ema3 	float64
+	Ema6 	float64
+	Ema9 	float64
 }
+
+type Pair struct {
+    Key   string
+    Value int
+}
+
+type PairList []Pair
 
 func main() {
 	c := make(chan os.Signal)
@@ -69,8 +81,16 @@ func main() {
 		}
 	}()
 
-	wg.Add(2)
+	CommonClient = new(client.CommonClient).Init(config.Host)
+	MarketClient = new(client.MarketClient).Init(config.Host)
+	AccountClient = new(client.AccountClient).Init(config.AccessKey, config.SecretKey, config.Host)
+	OrderClient = new(client.OrderClient).Init(config.AccessKey, config.SecretKey, config.Host)
+
+	marketSync()
+
+	wg.Add(3)
 	go goSymbolInfo()
+	go goMarket()
 	go goKline()
 	wg.Wait()
 }
@@ -80,27 +100,18 @@ func goSymbolInfo() {
 	wg.Done()
 }
 
-func symbolInfoSync() {
-	client := new(client.CommonClient).Init(config.Host)
-	resp, err := client.GetSymbols()
-	if err != nil {
-		exit = true
-		applogger.Error("Error: %s", err)
-	} else {
-		for _, result := range resp {
-			if result.QuoteCurrency != "usdt" {
-				continue
-			}
-			symbolInfo[result.Symbol] = result
-		}
-	}
+func goMarket() {
+	timeTickerChan := time.Tick(time.Second * 6)
+    for !exit {
+		marketSync()
+        <-timeTickerChan
+    }
+	wg.Done()
 }
 
 func goKline() {
-	timeTickerChan := time.Tick(time.Second * 1)
+	timeTickerChan := time.Tick(time.Second * 3)
     for !exit {
-		symbols := []string{"htusdt"};
-
 		num := len(symbols)
 		if num != 0 {
 			wgKline.Add(num)
@@ -114,111 +125,209 @@ func goKline() {
 	wg.Done()
 }
 
+func symbolInfoSync() {
+	resp, err := CommonClient.GetSymbols()
+	if err != nil {
+		exit = true
+		applogger.Error("GetSymbolsError: %s", err)
+	} else {
+		for _, result := range resp {
+			if result.QuoteCurrency != "usdt" {
+				continue
+			}
+			symbolInfo[result.Symbol] = result
+		}
+	}
+}
+
+func marketSync() {
+	resp, err := MarketClient.GetAllSymbolsLast24hCandlesticksAskBid()
+	if err != nil {
+		exit = true
+		applogger.Error("GetAllSymbolsLast24hCandlesticksAskBidError: %s", err)
+	} else {
+		symbolUp := make(map[string]int)
+		var tSymbols []string
+		for _, result := range resp {
+			if str4 := result.Symbol[len(result.Symbol)-4:]; str4 != "usdt" {
+				continue
+			}
+
+			regexp, _ := regexp.Compile(`([\d])[l|s]`)
+			match := regexp.MatchString(result.Symbol)
+			if match {
+				continue
+			}
+
+			open, _ := result.Open.Float64()
+			close, _ := result.Close.Float64()
+			upPre := int(close / open * 100)
+
+			if upPre > 100 {
+				symbolUp[result.Symbol] = upPre
+			}
+		}
+		sortedSymbol := sortMapByValue(symbolUp)
+
+		for _, symbol := range sortedSymbol[len(sortedSymbol) - 1:] {
+			tSymbols = append(tSymbols, symbol.Key)
+		}
+		symbols = tSymbols
+	}
+}
+
 func kline(symbol string) {
-	client := new(client.MarketClient).Init(config.Host)
 	num := CLOSENUM
 	init := true
-	if _, ok := symbolEma[symbol]; ok {
+	var emaList []Ema
+	if value, ok := symbolEma[symbol]; ok {
 		num = 1
 		init = false
-	} else {
-		symbolEmaTimes[symbol] = 0
-		symbolBuy[symbol] = 0
+		emaList = value
 	}
 
 	optionalRequest := market.GetCandlestickOptionalRequest{Period: market.MIN1, Size: num}
-	resp, err := client.GetCandlestick(symbol, optionalRequest)
+	resp, err := MarketClient.GetCandlestick(symbol, optionalRequest)
 	if err != nil {
 		exit = true
-		applogger.Error("Error: %s", err)
+		applogger.Error("GetCandlestickError: %s", err)
 	} else {
-		var buy = symbolBuy[symbol]
+		// abc := Ema{1, 1.2, 1.3, 1.3, 1.3}
+		// symbolEma["abc"] = append(symbolEma["abc"], abc)
+
+		// abc1 := Ema{2, 1.2, 1.3, 1.3, 1.3}
+		// symbolEma["abc"] = append(symbolEma["abc"], abc1)
+
+		// abc2 := Ema{3, 1.2, 1.3, 1.3, 1.3}
+		// symbolEma["abc"] = append(symbolEma["abc"], abc2)
+
+		// abc3 := Ema{4, 1.2, 1.3, 1.3, 1.3}
+		// symbolEma["abc"] = append(symbolEma["abc"][1:], abc3)
+
+		// applogger.Info("%+v", symbolEma)
+		// applogger.Info("%+v", symbolEma["abc"][2])
+	
+		// os.Exit(0)
+
+		// var buy = symbolBuy[symbol]
 
 		for _, result := range resp {
 			close, _ := result.Close.Float64()
 
-			var emaInfo Ema
-			if value, ok := symbolEma[symbol]; ok {
-				emaInfo = value
+			var preEma Ema
+			var idNew bool = true
+			var preEmaKey int
+			if count := len(emaList); count > 0 {
+				preEmaKey = count - 1
+				preEma = emaList[preEmaKey]
+				if preEma.Id == result.Id {
+					idNew = false
+					preEmaKey = count - 2
+					preEma = emaList[preEmaKey]
+				}
 			} else {
-				emaInfo = Ema{close, close, close, close, close, close}
+				preEma = Ema{result.Id, close, close, close, close}
 			}
 
-			currentEma := Ema{
-				2.0 / (39 + 1) * close + (39.0 - 1) / (39 + 1) * emaInfo.Ema39,
-				2.0 / (66 + 1) * close + (66.0 - 1) / (66 + 1) * emaInfo.Ema66,
-				2.0 / (93 + 1) * close + (93.0 - 1) / (93 + 1) * emaInfo.Ema93,
-				2.0 / (333+ 1) * close + (333.0- 1) / (333+ 1) * emaInfo.Ema333,
-				2.0 / (666+ 1) * close + (666.0- 1) / (666+ 1) * emaInfo.Ema666,
-				2.0 / (999+ 1) * close + (999.0- 1) / (999+ 1) * emaInfo.Ema999}
+			curEma := Ema{
+				result.Id,
+				close,
+				2.0 / (3 + 1) * close + (3.0 - 1) / (3 + 1) * preEma.Ema3,
+				2.0 / (6 + 1) * close + (6.0 - 1) / (6 + 1) * preEma.Ema6,
+				2.0 / (9 + 1) * close + (9.0 - 1) / (9 + 1) * preEma.Ema9}
 
-			symbolEma[symbol] = currentEma
+
+			if init {
+				emaList = append(emaList, curEma)
+			} else {
+				if idNew {
+					emaList = append(emaList[1:], curEma)
+				} else {
+					emaList[preEmaKey + 1] = curEma
+				}
+			}
+
+			applogger.Info("%+v", symbol)
+
+			applogger.Info("%+v, %+v, %+v", symbol, emaList, idNew)
+
+			// close, _ := result.Close.Float64()
+
+			// var emaInfo Ema
+			// if value, ok := symbolEma[symbol]; ok {
+			// 	emaInfo = value
+			// } else {
+			// 	emaInfo = Ema{0, close, close, close, close}
+			// }
+
+			// currentEma := Ema{
+			// 	close,
+			// 	2.0 / (36 + 1) * close + (36.0 - 1) / (36 + 1) * emaInfo.Ema3,
+			// 	2.0 / (63 + 1) * close + (63.0 - 1) / (63 + 1) * emaInfo.Ema6,
+			// 	2.0 / (96 + 1) * close + (96.0 - 1) / (96 + 1) * emaInfo.Ema9}
+
+			// symbolEma[symbol] = currentEma
+
+			// applogger.Info("%+v", symbolEma)
 
 			// if !init && symbolEmaTimes[symbol] >= CLOSENUM {
-			if !init && symbolEmaTimes[symbol] >= CLOSENUM {
-				// if buy == 0 && currentEma.Ema39 > currentEma.Ema66 && currentEma.Ema66 > currentEma.Ema93 && close > currentEma.Ema999 {
-				// if currentEma.Ema39 > currentEma.Ema66 && currentEma.Ema66 > currentEma.Ema93 {
-				if currentEma.Ema39 < currentEma.Ema66 && currentEma.Ema66 < currentEma.Ema93 {
-					if buy == 1 {
-						orderId := sellHandle(symbol, close)
-						if orderId != "" {
-							orderInfo := orderInfoHandle(orderId)
+			// 	if currentEma.Ema39 < currentEma.Ema66 && currentEma.Ema66 < currentEma.Ema93 {
+			// 		if buy == 1 {
+			// 			orderId := sellHandle(symbol, close)
+			// 			if orderId != "" {
+			// 				orderInfo := orderInfoHandle(orderId)
 					
-							realAmount := orderInfo.FilledAmount
-							if realAmount > 0 {
-								amount := 0.0
-								if value, ok := symbolAmount[symbol]; ok {
-									amount = value - 0.0001
-								}
-								symbolAmount[symbol], _ = strconv.ParseFloat(fmt.Sprintf("%."+strconv.Itoa(symbolInfo[symbol].AmountPrecision)+"f", amount - realAmount),64)
+			// 				realAmount := orderInfo.FilledAmount
+			// 				if realAmount > 0 {
+			// 					amount := 0.0
+			// 					if value, ok := symbolAmount[symbol]; ok {
+			// 						amount = value - 0.0001
+			// 					}
+			// 					symbolAmount[symbol], _ = strconv.ParseFloat(fmt.Sprintf("%."+strconv.Itoa(symbolInfo[symbol].AmountPrecision)+"f", amount - realAmount),64)
 		
-								minAmount, _ := symbolInfo[symbol].LimitOrderMinOrderAmt.Float64()
-								if symbolAmount[symbol] <= minAmount {
-									buy = 0
-								}
-							}
-						}
-					}
-				} else if currentEma.Ema333 > currentEma.Ema666 && currentEma.Ema666 > currentEma.Ema999 {
-					if buy == 0 {
-						orderId := buyHandle(symbol, close)
-						if orderId != "" {
-							orderInfo := orderInfoHandle(orderId)
+			// 					minAmount, _ := symbolInfo[symbol].LimitOrderMinOrderAmt.Float64()
+			// 					if symbolAmount[symbol] <= minAmount {
+			// 						buy = 0
+			// 					}
+			// 				}
+			// 			}
+			// 		}
+			// 	} else if currentEma.Ema333 > currentEma.Ema666 && currentEma.Ema666 > currentEma.Ema999 {
+			// 		if buy == 0 {
+			// 			orderId := buyHandle(symbol, close)
+			// 			if orderId != "" {
+			// 				orderInfo := orderInfoHandle(orderId)
 					
-							realAmount := orderInfo.FilledAmount
-							if realAmount > 0 {
-								amount := 0.0
-								if value, ok := symbolAmount[symbol]; ok {
-									amount = value
-								}
-								symbolAmount[symbol], _ = strconv.ParseFloat(fmt.Sprintf("%."+strconv.Itoa(symbolInfo[symbol].AmountPrecision)+"f", amount + realAmount),64)
-								minAmount, _ := symbolInfo[symbol].LimitOrderMinOrderAmt.Float64()
-								if symbolAmount[symbol] >= minAmount {
-									buy = 1
-								}
-							}
-						}
-					}
-				// } else if buy == 1 && currentEma.Ema39 < currentEma.Ema66 && currentEma.Ema66 < currentEma.Ema93 && currentEma.Ema93 < currentEma.Ema999 {
-				// } else if buy == 1 && close < currentEma.Ema999 && currentEma.Ema39 < currentEma.Ema66 && currentEma.Ema66 < currentEma.Ema93 {
-				// } else if currentEma.Ema333 < currentEma.Ema666 && currentEma.Ema666 < currentEma.Ema999 {
-				} 
-				symbolBuy[symbol] = buy
-			}
+			// 				realAmount := orderInfo.FilledAmount
+			// 				if realAmount > 0 {
+			// 					amount := 0.0
+			// 					if value, ok := symbolAmount[symbol]; ok {
+			// 						amount = value
+			// 					}
+			// 					symbolAmount[symbol], _ = strconv.ParseFloat(fmt.Sprintf("%."+strconv.Itoa(symbolInfo[symbol].AmountPrecision)+"f", amount + realAmount),64)
+			// 					minAmount, _ := symbolInfo[symbol].LimitOrderMinOrderAmt.Float64()
+			// 					if symbolAmount[symbol] >= minAmount {
+			// 						buy = 1
+			// 					}
+			// 				}
+			// 			}
+			// 		}
+			// 	} 
+			// 	symbolBuy[symbol] = buy
+			// }
 
-			if !init {
-				symbolEmaTimes[symbol] ++
+			// if !init {
+			// 	symbolEmaTimes[symbol] ++
 
-				applogger.Info("%s: %f %d %d %+v", symbol, close, symbolEmaTimes[symbol], buy, currentEma)
-			}
+			// 	applogger.Info("%s: %f %d %d %+v", symbol, close, symbolEmaTimes[symbol], buy, currentEma)
+			// }
 		}
 	}
+	symbolEma[symbol] = emaList
 	wgKline.Done()
 }
 
 func buyHandle(symbol string, price float64) (string) {
-	client := new(client.OrderClient).Init(config.AccessKey, config.SecretKey, config.Host)
-	
 	priceS := strconv.FormatFloat(price,'E',-1,64)
 
 	amountUsdt := float64(AMOUNTUSDT)
@@ -233,7 +342,7 @@ func buyHandle(symbol string, price float64) (string) {
 		Amount:    amount,
 	}
 
-	resp, err := client.PlaceOrder(&request)
+	resp, err := OrderClient.PlaceOrder(&request)
 	if err != nil {
 		exit = true
 		applogger.Error(err.Error())
@@ -254,8 +363,6 @@ func buyHandle(symbol string, price float64) (string) {
 }
 
 func sellHandle(symbol string, price float64) (string) {
-	client := new(client.OrderClient).Init(config.AccessKey, config.SecretKey, config.Host)
-	
 	priceS := strconv.FormatFloat(price,'E',-1,64)
 
 	amount := symbolAmount[symbol]
@@ -270,7 +377,7 @@ func sellHandle(symbol string, price float64) (string) {
 		Amount:    amountS,
 	}
 
-	resp, err := client.PlaceOrder(&request)
+	resp, err := OrderClient.PlaceOrder(&request)
 	if err != nil {
 		exit = true
 		applogger.Error(err.Error())
@@ -291,8 +398,7 @@ func sellHandle(symbol string, price float64) (string) {
 }
 
 func orderInfoHandle(orderId string) (OrderInfo) {
-	client := new(client.OrderClient).Init(config.AccessKey, config.SecretKey, config.Host)
-	resp, err := client.GetOrderById(orderId)
+	resp, err := OrderClient.GetOrderById(orderId)
 	if err != nil {
 		applogger.Error(err.Error())
 	} else {
@@ -315,3 +421,20 @@ func orderInfoHandle(orderId string) (OrderInfo) {
 
 	return OrderInfo{}
 }
+
+func sortMapByValue(m map[string]int) PairList {
+    p := make(PairList, len(m))
+    i := 0
+    for k, v := range m {
+        p[i] = Pair{k, v}
+		i++
+    }
+    sort.Sort(p)
+    return p
+}
+
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func (p PairList) Len() int           { return len(p) }
+
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
